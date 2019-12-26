28cfb1ac73db6620ac08dfcc6c727b8c70a27d16
Blakemcw <blakecmcwilliam@gmail.com>
2019-12-26 00:24:43 -0800

    Update permissions needed to make the SpotifyWebSDK usable and allow access to user's playlists

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-read-playback-state user-modify-playback-state streaming';
+  var scope = 'user-read-private playlist-read-private user-read-email user-read-playback-state user-modify-playback-state streaming';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
